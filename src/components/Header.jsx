6ac54bb24708b8699584abaa60883aa0561c38e4
Aryaa-969 <arya24si@mahasiswa.pcr.ac.id>
2026-06-05 14:53:56 +0700

    GTG

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,7 @@ export default function Header() {
                 {/* Profile Section */}
                 <div id="profile-container" className="flex items-center space-x-4 border-l pl-4 border-gray-300">
                     <span id="profile-text">
-                        Hello, <b>Arya Ibrahim</b>
+                        Hello bang ganteng, <b>Arya Ibrahim</b>
                     </span>
                     <img
                         id="profile-avatar"
